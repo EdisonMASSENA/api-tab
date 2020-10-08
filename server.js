465558df2f80b8d935c79493fd52bed7359cad8e
Edison MASSENA <massenaedison0@outlook.fr>
2020-10-08 17:47:11 +0200

    make it better

--- server.js
+++ server.js
@@ -29,10 +29,11 @@ const cors = require("cors");
 const db = require('./app/models');
 const path = require('path');
 const app = express();
+
 var distDir = __dirname + "/dist/";
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(distDir));
-const port = process.env.PORT || 3333;
+
 app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -49,9 +50,10 @@ require("./app/routes/tableau.routes")(app);
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js,Express, and Postgres API' });
 });
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 
 
 // const express = require("express");
