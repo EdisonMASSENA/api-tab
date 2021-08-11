8666722329f2473a2ef8f5a491c41a6b8dd28434
Edison MASSENA <massenaedison0@outlook.fr>
2021-08-11 10:43:33 +0200

    clean

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-    // HOST: "ec2-52-209-171-51.eu-west-1.compute.amazonaws.com",
-    // USER: "lmgwokhihrakkg",
-    // PASSWORD: "6ae5d15b4d1845a56901f3db49cfb933e3768efe0d20947d3fb5c3e84869cf76",
-    // DB: "de6gsk2p6gqq73",
-    // dialect: "postgres",
-    // pool: {
-    //   max: 5,
-    //   min: 0,
-    //   acquire: 30000,
-    //   idle: 10000
-    // },
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "tabprojet",
-    dialect: "mysql",
+    HOST: "ec2-52-209-171-51.eu-west-1.compute.amazonaws.com",
+    USER: "lmgwokhihrakkg",
+    PASSWORD: "6ae5d15b4d1845a56901f3db49cfb933e3768efe0d20947d3fb5c3e84869cf76",
+    DB: "de6gsk2p6gqq73",
+    dialect: "postgres",
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000
     },
+    // HOST: "localhost",
+    // USER: "root",
+    // PASSWORD: "",
+    // DB: "tabprojet",
+    // dialect: "mysql",
+    // pool: {
+    //   max: 5,
+    //   min: 0,
+    //   acquire: 30000,
+    //   idle: 10000
+    // },
   };
