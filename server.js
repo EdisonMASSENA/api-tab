fb641bc3fe6355d37b24d33a50bb92819b43bb56
Edison MASSENA <massenaedison0@outlook.fr>
2022-01-25 10:32:27 +0100

    syncsequ

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// db.sequelize.sync();
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
+db.sequelize.sync();
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
 
 
 require('./app/routes/auth.routes')(app);
