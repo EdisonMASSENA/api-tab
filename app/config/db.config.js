7e0ec6fa40502d6f1683925f174247dbbb239462
Edison MASSENA <massenaedison0@outlook.fr>
2022-09-02 11:56:52 +0200

    sync

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   
 // dev
-    // HOST: "ec2-34-241-90-235.eu-west-1.compute.amazonaws.com",
-    // USER: "uedxiamllnnoph",
-    // PASSWORD: "90608e0cbf0503a9c2d3a88caecce3666fd5d57956987259f2c036e6f07916df",
-    // DB: "d9mv795rjb4dmu",
-    // dialect: "postgres",
-    // pool: {
-    //   max: 5,
-    //   min: 0,
-    //   acquire: 30000,
-    //   idle: 10000
-    // },
+    HOST: "ec2-34-241-90-235.eu-west-1.compute.amazonaws.com",
+    USER: "uedxiamllnnoph",
+    PASSWORD: "90608e0cbf0503a9c2d3a88caecce3666fd5d57956987259f2c036e6f07916df",
+    DB: "d9mv795rjb4dmu",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
 
 // // prod
 //     HOST: "ec2-52-209-171-51.eu-west-1.compute.amazonaws.com",
@@ -26,16 +26,16 @@ module.exports = {
 //       idle: 10000
 //     },
 
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "tabprojet",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
+    // HOST: "localhost",
+    // USER: "root",
+    // PASSWORD: "root",
+    // DB: "tabprojet",
+    // dialect: "mysql",
+    // pool: {
+    //   max: 5,
+    //   min: 0,
+    //   acquire: 30000,
+    //   idle: 10000
+    // },
 
   };
